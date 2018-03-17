717dd60fcc43f6571adc0a76971dc0991ab9c1a8
bessyRamos <bessyramos3@gmail.com>
2018-03-17 17:52:19 -0600

    insert de datos

--- app/src/main/java/unah/proyecto/aeo/aplicacionagendaelectronicaoriental/ConexionSQLiteHelper.java
+++ app/src/main/java/unah/proyecto/aeo/aplicacionagendaelectronicaoriental/ConexionSQLiteHelper.java
@@ -57,9 +57,9 @@ public class ConexionSQLiteHelper extends SQLiteOpenHelper {
             " 1, 1,2, 4)," +
             "(4,'Centro Tecnico Vocacional Pedro Nufio','2763-3195','', 'Barrio El Quiquisque', '', 'Instituto Público', '14.011955', '-86.569433'," +
             " 1, 2,2, 0703)," +
-            "(5,'Instituto Teodoro Rodas Valle','2763-3050','3219-6497', 'Barrio las Flores', 'loborams@yahoo.com', 'Instituto privado','',''," +
+            "(5,'Instituto Teodoro Rodas Valle','2763-3050','3219-6497', 'Barrio las Flores', 'loborams@yahoo.com', 'Instituto Privado','',''," +
             " 1, 2,2, 3),"+
-            "(6,'Instituto Técnico Alegandro Flores','2793-4123','', 'Col.Los Montesitos, Salida Danlí', '', 'Instituto Público',13.874814 ,-86.554105," +
+            "(6,'Instituto Técnico Alejandro Flores','2793-4123','', 'Col.Los Montesitos, Salida Danlí', '', 'Instituto Público',13.874814 ,-86.554105," +
             " 1, 2,2, 4),"+
             "(7,'Farmacia Cimar','2763-2115','', 'Barrio San Sebastian', '', 'Cuenta con servicio a domicilio','',''," +
             " 1, 3,2, 4),"+
@@ -69,31 +69,32 @@ public class ConexionSQLiteHelper extends SQLiteOpenHelper {
             " 1, 3,2, 3),"+
             "(10,'BANHCAFE','2763-2402','', 'Barrio Tierra Blanca', '', 'Servicios financieros','14.033798','-86.56784'," +
             " 1, 4, 2, 3),"+
-            "(11,'Banco Atlantida','2793-4200','', 'El paraiso frente a plaza Ramón Ignacio Díaz, Bo.San Isidro', '', 'Servicios financieros','',''," +
+            "(11,'Banco Atlántida','2793-4200','', 'El paraíso frente a plaza Ramón Ignacio Díaz, Bo.San Isidro', '', 'Servicios financieros','',''," +
             " 1, 4, 2, 4),"+
             "(12,'Hotel La Esperanza','2763-2106','', 'Bo.Tierra Santa', '', 'Servicios de hoteleria', '14.02976','-86.570383'," +
             " 1, 5, 2, 3),"+
             "(13,'Hotel y Restaurante Mario Chávez','2793-4345','', 'Calle principal quinta avenida, El Paraíso', '', 'Servicios de hoteleria y comida','13.864908' ,'-86.55265'," +
             " 1, 5, 2, 4),"+
-            "(14,'Finca Santa Emilia','','9842-7586', 'Siete km del parque central de Danlí, una reserva natural', '', 'turismo','14.025889','-86.540083'," +
+            "(14,'Finca Santa Emilia','','9842-7586', 'Siete km del parque central de Danlí, una reserva natural', '', 'Turismo','14.025889','-86.540083'," +
             " 1, 5, 2, 3),"+
-            "(15,'Instituto de la Propiedad','2763-0473','', 'Bo.Oriental', '', 'Registro de bienes','',''," +
+            "(15,'Instituto de la Propiedad','2763-0473','', 'Bo.Oriental', '', 'Registro de Bienes','',''," +
             " 1, 6, 2, 3),"+
-            "(16,'Secretaria Agricultura Ganaderia','2763-2112','98543113', 'Salida El Paraiso frente al intituto Cosme Garcia', 'valdivita_10@yahoo.com', ' asesoria ', '14.023419','-86.569327'," +
+            "(16,'Secretaría Agricultura Ganaderia','2763-2112','98543113', 'Salida El Paraíso frente al instituto Cosme Garcia', 'valdivita_10@yahoo.com', ' Asesoría ', '14.023419','-86.569327'," +
             " 1, 6, 2, 3),"+
-            "(17,'Plaza Unicentro','2763-3552','', 'Bo.el centro calle del comerciofrente a hondutel', 'jeany_salinas@hotmail.com', 'adquiere un bien','14.031468', '-86.571082'," +
+            "(17,'Plaza Unicentro','2763-3552','', 'Bo.el centro calle del comercio frente a hondutel', 'jeany_salinas@hotmail.com', 'adquiere un bien','14.031468', '-86.571082'," +
             " 1, 7, 2, 3),"+
-            "(18,'Carrión','2763-2331','', 'frente a Casa de la Cultura', '', 'servio de bienes','14.03006' ,'-86.567591'," +
+
+            "(18,'Carrión','2763-2331','', 'frente a Casa de la Cultura', '', 'Servio de Bienes','14.03006' ,'-86.567591'," +
             " 1, 7, 2, 3),"+
             "(19,'Oficina Postal Danlí Paraíso','2763-2036','', 'Bo. El Centro frente al Parque Centenario', '', ' envio','14.030837','-86.568162'," +
             " 1, 8, 2, 3),"+
             "(20,'Cámara de Comercio E Industrias de Danlí (CCID)','2763-2061','', 'Colonia Gualiquemes por terminal de buses contiguo ala Plaza San Miguel en Boulevard La Democracia.', 'cidedanli@yahoo.com cidedanli@hotmail.com', ' Comercio','',''," +
             " 1, 8, 2, 3),"+
-            "(21,'Inmobiliaria y Constructora El Protector','27634746','', 'Media cuadra al norte del parque central entre banco Los Trabajadores y La Despensa (72,02 km)', '','Agente de bienes raíces','',''," +
+            "(21,'Inmobiliaria y Constructora El Protector','27634746','', 'Media cuadra al norte del parque central entre banco Los Trabajadores y La Despensa (72,02 km)', '',' Bienes Raíces','',''," +
             " 1, 9, 2, 3),"+
             "(22,'Despacho Legal Lic. Hector Paguaga','','9767-3875', 'Barrio Tierra Blanca', '', ' Abogado','',''," +
             " 1, 10, 2, 3),"+
-            "(23,'Colegio de Abogados Capitulo Danli','','', 'Frente al Triangulo salida al paraiso a la par de Instituto Técnico Pedro Nufio', '', '','14.014148','-86.569564'," +
+            "(23,'Colegio de Abogados Capitulo Danlí','','', 'Frente al Triangulo salida al paraíso a la par de Instituto Técnico Pedro Nufio', '', '','14.014148','-86.569564'," +
             " 1, 10, 2, 3),"+
             "(24,'FUNERALES DIVINA MISERICORDIA','2763-6798','', 'V-216  Danlí', '', 'disponibilidad de local', '14.019116','-86.569615'," +
             " 1, 11, 2, 3),"+
@@ -105,7 +106,7 @@ public class ConexionSQLiteHelper extends SQLiteOpenHelper {
             " 1, 2, 2, 3),"+
             "(28,'Escuela Francisco Morazán','2793-4263','', 'Bo. Santa Clara', '', 'Escuela','13.864001','-86.555687'," +
             " 1, 2, 2, 4),"+
-            "(29,'UNAH-Tec Danlí','2763-9900','8935-3296', 'Carretera a Danlí El Paraíso frente al hospital Gabriela Alvarado', 'tecdanli@unah.edu.hn ', 'Universidad', '13.992873'  ,'-86.570329'," +
+            "(29,'UNAH-Tec Danlí','2763-9900','8935-3296', 'Carretera a Danlí El Paraíso frente al Hospital Gabriela Alvarado', 'tecdanli@unah.edu.hn ', 'Universidad', '13.992873'  ,'-86.570329'," +
             " 1, 2, 2, 3),"+
             "(30,' BANADESA','2763-2393','', 'Bo.Tierra Blanca Frente al Museo Municipal', '', 'Banco','14.032018','-86.566156'," +
             " 1, 4, 2, 3),"+
@@ -113,7 +114,7 @@ public class ConexionSQLiteHelper extends SQLiteOpenHelper {
             " 1, 4, 2, 4),"+
             "(32,'Hotel La Casona','',' 9492-8526', 'Cuesta el Dorado salida a Tegucigalpa', '', 'Hotel','14.025642', '-86.585041'," +
             " 1, 5, 2, 3),"+
-            "(33,'Los Llanos Restaurante Turístico','','', 'El Paraiso El Paraiso', '', 'Hotel','14.073693','-86.418731'," +
+            "(33,'Los Llanos Restaurante Turístico','','', 'El Paraíso El Paraíso', '', 'Hotel','14.073693','-86.418731'," +
             " 1, 5, 2, 4),"+
             "(34,'IHMA Danlí','','', 'Las Colinas', '', 'Instituciones Públicas','14.022516','-86.568433'," +
             " 1, 6, 2, 3),"+
@@ -125,7 +126,7 @@ public class ConexionSQLiteHelper extends SQLiteOpenHelper {
             "1,8,2,3),"+
             "(38,'Comercial la Unión','2763-2357','','Bo. Abajo','','Comercial','','',"+
             "1,7,2,3),"+
-            "(39,'Residencial Vereda Real','2763-6274','99782015','Crretera panamericana entrada principal Col.La Ceibita 100 metros a mano derecha de la Iglesia Gran Comision','@residencialveredarealdanli','Bienes y Raises','14.015085','-86.574649',"+
+            "(39,'Residencial Vereda Real','2763-6274','99782015','Carretera panamericana entrada principal Col.La Ceibita 100 metros a mano derecha de la Iglesia Gran Comision','@residencialveredarealdanli','Bienes y Raises','14.015085','-86.574649',"+
             "1,9,2,3),"+
             "(40,'El Rincón del Toro','2763-4312','','Los Zarsales','',' Comercio de Bienes','14.027979','-86.577733',"+
             "1,6,2,3)";
