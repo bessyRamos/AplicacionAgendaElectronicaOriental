c16b71b0f236ad66361ce5df3bec1bd84ae55236
SheyliSanchez <vanessa1994sanchez@hotmail.com>
2018-04-12 16:53:02 -0600

    Perfil de organizaciones-web service

---
+++ app/src/main/java/unah/proyecto/aeo/aplicacionagendaelectronicaoriental/clasesJAVASheyli/RecuperacionDePassword.java
@@ -0,0 +1,13 @@
+package unah.proyecto.aeo.aplicacionagendaelectronicaoriental.clasesJAVASheyli;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecuperacionDePassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recuperacion_de_password);
+    }
+}
